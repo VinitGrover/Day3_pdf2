6d622ff91b18d9ce8e8312ab7b43dc5ebffd8cad
VinitGrover <vinitgrover255@gmail.com>
2022-05-22 11:32:09 +0530

    Adding uc2

--- LineProblem.java
+++ LineProblem.java
@@ -1,7 +1,21 @@
 package com.bridgelabz;
 
+import java.lang.Math;
+
 public class LineProblem {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program on Master Branch");
+
+        double x1 = 40.0, x2 = 52.0, y1 = 63.0, y2 = 77.0;
+        double a = Math.sqrt(x2 - x1);
+        double b = Math.sqrt(y2 - y1);
+
+        System.out.println(a + " " + b);
+
+        Double d1 = Double.valueOf(a);
+        Double d2 = Double.valueOf(b);
+
+        System.out.println("Are the Lengths equal : "+ d1.equals(d2));
+
     }
 }
